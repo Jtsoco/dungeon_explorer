ee276dc6aef4a5a1aba1c09ce09ade520b3fe6e6
Jackson Socolofsky <jtsoco@gmail.com>
2026-01-12 06:16:56 +0900

    updated player controller to send block and stop block input events, also to use set instead of list for input events

--- entity/controllers/player_controller.py
+++ entity/controllers/player_controller.py
@@ -5,34 +5,37 @@ import pyxel
 
 class PlayerController():
     def __init__(self):
-        self.recent_movement = []
+        self.recent_movement = set()
 
     def poll_events(self):
-        new_recents = []
-        rl_movement = []
+        new_recents = set()
+        rl_movement = set()
         events = []
         if pyxel.btn(pyxel.KEY_LEFT):
-            rl_movement.append(InputEvent(IE.MOVE, direction=DS.LEFT))
+            rl_movement.add(InputEvent(IE.MOVE, direction=DS.LEFT))
         if pyxel.btn(pyxel.KEY_RIGHT):
-            rl_movement.append(InputEvent(IE.MOVE, direction=DS.RIGHT))
+            rl_movement.add(InputEvent(IE.MOVE, direction=DS.RIGHT))
         if pyxel.btn(pyxel.KEY_SPACE):
-            new_recents.append(InputEvent(IE.JUMP))
+            new_recents.add(InputEvent(IE.JUMP))
         if pyxel.btn(pyxel.KEY_D):
-            new_recents.append(InputEvent(IE.ATTACK))
+            new_recents.add(InputEvent(IE.ATTACK))
         if pyxel.btn(pyxel.KEY_S):
-            new_recents.append(InputEvent(IE.BLOCK))
+            new_recents.add(InputEvent(IE.BLOCK))
+        else:
+            new_recents.add(InputEvent(IE.STOP_BLOCK))
+
 
         # for now, polling for rl movement as a quick bug fix
         if not rl_movement or (len(rl_movement) == 2):
-            new_recents.append(InputEvent(IE.STOP_MOVE))
+            new_recents.add(InputEvent(IE.STOP_MOVE))
         else:
-            new_recents.extend(rl_movement)
+            new_recents.update(rl_movement)
         # this checks if there are any new events since last time
         # only turning new events into events to return
         # events = [event for event in new_recents if event not in self.recent_movement]
-        new_recent_movements = []
+        new_recent_movements = set()
         for event in new_recents:
-            new_recent_movements.append((event.input_type, event.direction))
+            new_recent_movements.add((event.input_type, event.direction))
             if (event.input_type, event.direction) not in self.recent_movement:
                 events.append(event)
 
